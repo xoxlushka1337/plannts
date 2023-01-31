d00fcaf3ab716c9a76d42ed14ce124e856c5ae65
xoxlushka1337 <ulana.xoxlushka@gmail.com>
2023-01-31 15:41:24 +0300

    feat: prices accordion js

--- plants/index.js
+++ plants/index.js
@@ -81,3 +81,37 @@ function app() {
   });
 }
 app();
+
+//prices accordion
+
+document.addEventListener('DOMContentLoaded', () => {
+  const accordions = document.querySelectorAll('.prices__lining');
+
+  let openedAccordion = null;
+  accordions.forEach((el) => {
+    el.addEventListener('click', (e) => {
+      const self = e.currentTarget;
+
+      if (openedAccordion == null) {
+        self.classList.add('open');
+        openedAccordion = self;
+        return;
+      }
+
+      if (!self.classList.contains('open')) {
+        openedAccordion.classList.remove('open');
+        self.classList.add('open');
+        openedAccordion = self;
+      } else {
+        openedAccordion.classList.remove('open');
+        openedAccordion = null;
+      }
+    });
+  });
+  const buttonOrders = document.querySelectorAll('.prices__order');
+  buttonOrders.forEach((el) => {
+    el.addEventListener('click', (e) => {
+      e.stopPropagation();
+    });
+  });
+});
